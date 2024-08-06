04e5566161c6f92bf3beda3b1838d57ebeed48cd
Cubby Alexander <hall.j.alexander@gmail.com>
2024-08-06 16:57:32 -0400

    Accesses text file and generates hand / card objects

---
+++ lib/card.rb
@@ -0,0 +1,21 @@
+class Card
+  include Comparable
+
+  attr_reader :value, :suit
+
+  VALUES = ('2'..'9').to_a + %w(T J Q K A)
+  SUITS = %w(H D C S)
+
+  def initialize(card_str)
+    @value = card_str[0]
+    @suit = card_str[1]
+  end
+
+  def value_rank
+    VALUES.index(value)
+  end
+
+  def <=>(other)
+    value_rank <=> other.value_rank
+  end
+end
--- main.rb
+++ main.rb
@@ -1,1 +1,10 @@
-puts "Hello World!"
+require_relative 'lib/poker'
+
+puts "Evaluating poker hands in poker.txt..."
+
+hand_file_path = 'poker.txt'
+
+poker_evaluation = Poker.new(hand_file_path)
+player1_total_wins = poker_evaluation.play
+
+puts "Player 1 won a total of #{player1_total_wins} hands."
