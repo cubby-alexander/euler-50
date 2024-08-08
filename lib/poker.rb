19c07851d5a6826cfcf2008539dd675b6029cf09
Cubby Alexander <hall.j.alexander@gmail.com>
2024-08-08 16:44:26 -0400

    Resolves player hand details (type and high cards)

--- lib/poker.rb
+++ lib/poker.rb
@@ -14,7 +14,7 @@ class Poker
       player1_hand = Hand.new(cards[0, 5], 'Player 1')
       player2_hand = Hand.new(cards[5, 5], 'Player 2')
 
-      puts "Player 1 hand: #{player1_hand.hand_cards}, Player 2 hand: #{player2_hand.hand_cards}"
+      puts "Player 1 hand: #{player1_hand.hand_cards} #{player1_hand.hand_details[:type]}, Player 2 hand: #{player2_hand.hand_cards} #{player2_hand.hand_details[:type]}"
       player1_wins += 1
     end
 
